075a5cfc3eb06f0a220dd8a94188c33de0103c32
mubashirnazir <mubashirnazir42@gmail.com>
2024-06-04 05:40:09 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("hi")
+
+favouriteMovieGenre("space")
+
+favouriteMode("dark")
+
+favouriteFruit("banana")
+
+
+function setProp(prop,value){
+    document.documentElement.style.setProperty(prop,value)
+}
+
